55f6e938062d0e400ec8c4c1a7b5c19cbc3cd33a
FranciscoNoam <antoenjara1998@gmail.com>
2024-04-15 14:50:28 +0300

    Écriture d'un bean CDI GestionnaireCompte

---
+++ src/main/java/mg/antoenjara/tpbanque/service/GestionnaireCompte.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mg.antoenjara.tpbanque.service;
+import jakarta.enterprise.context.RequestScoped;
+/**
+ *
+ * @author francisco
+ */
+@RequestScoped
+public class GestionnaireCompte {
+    
+}
