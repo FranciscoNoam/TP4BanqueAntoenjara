0a5079ae99034f07b0af4253628a217db8b13926
FranciscoNoam <antoenjara1998@gmail.com>
2024-04-16 11:40:33 +0300

    Code du bean CDI

--- src/main/java/mg/antoenjara/tpbanque/service/GestionnaireCompte.java
+++ src/main/java/mg/antoenjara/tpbanque/service/GestionnaireCompte.java
@@ -3,36 +3,38 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package mg.antoenjara.tpbanque.service;
+
 import jakarta.annotation.sql.DataSourceDefinition;
 import jakarta.enterprise.context.RequestScoped;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
-import jakarta.persistence.Query;
+import jakarta.persistence.TypedQuery;
 import jakarta.transaction.Transactional;
 import java.util.List;
 import mg.antoenjara.tpbanque.entity.CompteBancaire;
+
 /**
  *
  * @author francisco
  */
-@DataSourceDefinition (
-    className="com.mysql.cj.jdbc.MysqlDataSource",
-    name="java:app/jdbc/banque",
-    serverName="localhost",
-    portNumber=3306,
-    user="banque",              // nom et
-    password="banque", // mot de passe que vous avez donnés lors de la création de la base de données
-    databaseName="banque",
-    properties = {
-      "useSSL=false",
-      "allowPublicKeyRetrieval=true",
-      "driverClass=com.mysql.cj.jdbc.Driver"
-    }
+@DataSourceDefinition(
+        className = "com.mysql.cj.jdbc.MysqlDataSource",
+        name = "java:app/jdbc/banque",
+        serverName = "localhost",
+        portNumber = 3306,
+        user = "banque", // nom et
+        password = "banque", // mot de passe que vous avez donnés lors de la création de la base de données
+        databaseName = "banque",
+        properties = {
+            "useSSL=false",
+            "allowPublicKeyRetrieval=true",
+            "driverClass=com.mysql.cj.jdbc.Driver"
+        }
 )
 
 @RequestScoped
 public class GestionnaireCompte {
-    
+
     @PersistenceContext(unitName = "banquePU")
     private EntityManager em;
 
@@ -45,17 +47,18 @@ public class GestionnaireCompte {
     public void persist(CompteBancaire compte) {
         em.persist(compte);
     }
-    
+
     public List<CompteBancaire> getAllComptes() {
-      Query query = em.createNamedQuery("CompteBancaire.findAll");
-       return query.getResultList();
+        String requete = "SELECT c FROM COMPTEBANCAIRE c ";
+        TypedQuery<CompteBancaire> query =em.createQuery(requete, CompteBancaire.class); 
+        List<CompteBancaire> liste = query.getResultList();
+        return liste;
     }
 
     @Transactional
     public void creerCompte(CompteBancaire c) {
-      em.persist(c);
-      
+        em.persist(c);
+
     }
-    
 
 }
